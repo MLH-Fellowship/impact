7d7da6363b9be1997dceea0f8299e193a8da02f9
Lily Tang <lilyytang@gmail.com>
2020-12-09 11:32:24 -0800

    donate and see donations in profile works

--- client/src/Components/PaymentTable.js
+++ client/src/Components/PaymentTable.js
@@ -1,49 +1,60 @@
 import { useEffect, useState } from 'react';
-import { Table } from 'antd';
+import { Table, Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
 import api from '../Scripts/api';
 
-const columns = [
-  {
-    title: "Organization",
-    dataIndex: "orgid",
-    key: "orgid",
-  },
-  {
-    title: "Freq",
-    dataIndex: "freq",
-    key: "freq",
-  },
-  {
-    title: "Value ($)",
-    dataIndex: "value",
-    key: "value",
-    sorter: (a, b) => a.age - b.age
-  },
-  {
-    title: "Recurrence",
-    dataIndex: "freq",
-    key: "freq",
-  },
-  {
-    title: "Status",
-    dataIndex: "active",
-    key: "active",
-  }
-];
 
 function PaymentTable() {
   const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+  const columns = [
+    {
+      title: "Organization",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Freq",
+      dataIndex: "freq",
+      key: "freq",
+    },
+    {
+      title: "Value ($)",
+      dataIndex: "value",
+      key: "value",
+      sorter: (a, b) => a.age - b.age
+    },
+    {
+      title: "Recurrence",
+      dataIndex: "freq",
+      key: "freq",
+    },
+    {
+      title: "Status",
+      dataIndex: "active",
+      key: "active",
+    }
+  ];
 
   useEffect(() => {
     async function getData() {
       try {
         let newData = await api.getPayments();
+        let orgLookup = await api.getOrgs();
+
+        console.log(newData);
+        console.log(orgLookup);
 
         for (var i = 0; i < newData.length; i++) {
           newData[i].active = newData[i].active ? 'Active' : 'Paused';
+          newData[i].name = orgLookup.find(x => x._id === newData[i].orgid).name;
         }
+        console.log(newData);
 
         setData(newData);
+        setLoading(false);
       } catch (error) {
         console.error(error);
       }
@@ -53,7 +64,9 @@ function PaymentTable() {
   }, [])
 
   return (
-    <Table columns={columns} dataSource={data} />
+    <Spin indicator={antIcon} spinning={loading}>
+      <Table columns={columns} dataSource={data} />
+    </Spin>
   )
 }
 
