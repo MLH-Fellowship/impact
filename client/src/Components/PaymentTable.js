80eafee524415553c94f548e0a595c412478bc16
Lily Tang <lilyytang@gmail.com>
2020-12-09 14:58:42 -0800

    add redirect and show correct data to user

--- client/src/Components/PaymentTable.js
+++ client/src/Components/PaymentTable.js
@@ -2,11 +2,13 @@ import { useEffect, useState } from 'react';
 import { Table, Spin, Button, message } from 'antd';
 import { LoadingOutlined } from '@ant-design/icons';
 import api from '../Scripts/api';
+import useUser from "../Scripts/userManager";
 
 
 function PaymentTable() {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
+  const { user, setUser } = useUser();
   const antIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
 
   const onDelete = (orgid) => {
@@ -36,9 +38,7 @@ function PaymentTable() {
     {
       title: "Value ($)",
       dataIndex: "value",
-      key: "value",
-      sorter: (a, b) => a.value - b.value,
-      sortDirections: ['descend', 'ascend']
+      key: "value"
     },
     {
       title: "Recurrence",
@@ -63,17 +63,17 @@ function PaymentTable() {
       try {
         let newData = await api.getPayments();
         let orgLookup = await api.getOrgs();
-
         console.log(newData);
-        console.log(orgLookup);
 
         for (var i = 0; i < newData.length; i++) {
           newData[i].active = newData[i].active ? 'Active' : 'Paused';
           newData[i].name = orgLookup.find(x => x._id === newData[i].orgid).name;
           newData[i].website = orgLookup.find(x => x._id === newData[i].orgid).website;
         }
+        
+        // Get data for only current user
+        newData = newData.filter(x => x.userid == user.userID);
         console.log(newData);
-
         setData(newData);
         setLoading(false);
       } catch (error) {
